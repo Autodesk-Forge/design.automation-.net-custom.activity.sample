3a59625224b00a05c34e0dcbd54a84c6fcd1872d
miaoj <jonathan.miao@autodesk.com>
2015-10-19 10:59:29 -0700

    sync from Developer-Autodesk and update
    There was a submission directly to Developer-Autodesk to fix a bug (SHA
    a0456de). That submission was not synced to my branch so that my branch
    works for V2 but main branch doesn't.
    Now sync it again and fix this issue.

--- Client/Program.cs
+++ Client/Program.cs
@@ -136,7 +136,7 @@ namespace Client
                 package.Resource = url;
                 container.UpdateObject(package);
             }
-            container.SaveChanges(System.Data.Services.Client.SaveChangesOptions.PatchOnUpdate);
+            container.SaveChanges();
             return package;
         }
 
